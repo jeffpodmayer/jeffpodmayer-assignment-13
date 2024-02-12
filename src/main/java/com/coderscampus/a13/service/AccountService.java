dce0c4fcb5c9752b8672def2dc778a8aa30d9ab0
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-02-12 10:22:39 -0800

    updated html files

--- src/main/java/com/coderscampus/a13/service/AccountService.java
+++ src/main/java/com/coderscampus/a13/service/AccountService.java
@@ -53,6 +53,4 @@ public class AccountService {
 		user.getAccounts().add(account);
 		saveAccount(account);
 	}
-
-
 }
--- src/main/java/com/coderscampus/a13/web/UserController.java
+++ src/main/java/com/coderscampus/a13/web/UserController.java
@@ -1,5 +1,6 @@
 package com.coderscampus.a13.web;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
