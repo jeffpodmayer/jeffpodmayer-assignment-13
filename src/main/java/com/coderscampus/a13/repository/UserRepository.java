b562676ed14f10cfe7952269c2a54e82badf1386
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-02-12 11:59:09 -0800

    wrote custom query

--- src/main/java/com/coderscampus/a13/repository/UserRepository.java
+++ src/main/java/com/coderscampus/a13/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.coderscampus.a13.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.coderscampus.a13.domain.User;
@@ -8,5 +11,6 @@ import com.coderscampus.a13.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-	
+	@Query("select u from User u left join fetch u.accounts left join fetch u.address")
+	List<User> findAll();
 }
--- src/main/java/com/coderscampus/a13/web/UserController.java
+++ src/main/java/com/coderscampus/a13/web/UserController.java
@@ -1,6 +1,5 @@
 package com.coderscampus.a13.web;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
